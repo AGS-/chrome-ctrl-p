c9cc4518fb9ba565dd83eb558b3c0861f4dfdd7b
angel.gomez <angelegomezsd@gmail.com>
2017-03-25 22:09:03 -0700

    Temporarily disabling results

--- client/js/components/App.jsx
+++ client/js/components/App.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import SearchBar from './SearchBar.jsx';
-import SearchResults from './SearchResults.jsx';
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
         <SearchBar />
-        <SearchResults />
       </div>
     );
   }
